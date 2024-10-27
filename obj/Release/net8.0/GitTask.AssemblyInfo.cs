671e6ff2b9f99b30a05b0c8064ae561b7a400232
JavidGasim <gasimovcavid777@gmail.com>
2024-10-27 14:32:38 +0400

    somechanges

--- obj/Release/net8.0/GitTask.AssemblyInfo.cs
+++ obj/Release/net8.0/GitTask.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitTask")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8fc86507e55e4381d3b187711d89aa98fc7e24e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitTask")]
